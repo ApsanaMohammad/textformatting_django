5b82cdd0392a72935b56af66f4b7ec1fe762749a
ApsanaMohammad <apsanamohammad777@gmail.com>
2024-01-08 17:09:28 +0530

    completed

--- mysite/views.py
+++ mysite/views.py
@@ -6,11 +6,11 @@ from django.http import HttpResponse
 def index(request):
     return render(request,'about.html')
 def removepunc(request):
-    dt = request.GET.get('t', 'default')
-    che=request.GET.get('rmpunc','off')
-    lc=request.GET.get('lc','off')
-    uc=request.GET.get('uc','off')
-    rmln=request.GET.get('rmln','off')
+    dt = request.POST.get('t', 'default')
+    che=request.POST.get('rmpunc','off')
+    lc=request.POST.get('lc','off')
+    uc=request.POST.get('uc','off')
+    rmln=request.POST.get('rmln','off')
     
     punctuations='''!@#$%^&*()_+{}:"?><[];',./'''
     if(che=='on'):
@@ -19,25 +19,28 @@ def removepunc(request):
              if char not in punctuations:
                  analysed=analysed+char
          params={'purpose':'Removing punctuations','analyse':analysed} 
-    elif(lc=='on'): 
+         dt=analysed
+    if(lc=='on'): 
          analysed = "" 
          for char in dt:
                  analysed=analysed+char.lower()
          params={'purpose':'Making Lower Case','analyse':analysed}
-
-    elif(uc=='on'): 
+         dt=analysed
+    if(uc=='on'): 
          analysed = "" 
          for char in dt:
                  if char!="\n":
                    analysed=analysed+char.upper()
          params={'purpose':'Making Upper Case','analyse':analysed} 
-    elif(rmln=='on'): 
+         dt=analysed
+    if(rmln=='on'): 
          analysed = "" 
          for char in dt:
              if char not in punctuations:
                  analysed=analysed+char.upper()
-         params={'purpose':'Removing NewLine','analyse':analysed}         
-    else:
-         params={'purpose':'NO CHANGES APPLIES TO YOUR TEXT','analyse':analysed}             
+         params={'purpose':'Removing NewLine','analyse':analysed}     
+         dt=analysed    
+       
+    params={'purpose':'Removing NewLine','analyse':analysed}              
     return render(request,'analyse.html',params)
 
